42dcdff975e1f7b40a1cf5e2758b76df82a50d8c
saitoiotken <saitoiotken@users.noreply.github.com>
2026-05-07 09:52:33 +0900

    Add file

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "GoogleMobileAdsMediationFluct",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,15 +13,33 @@ let package = Package(
             targets: ["GoogleMobileAdsMediationFluct"]
         ),
     ],
+    dependencies: [
+        .package(
+          url: "https://github.com/voyagegroup/FluctSDK-iOS-Swift-Package.git",
+          exact: "6.42.1"
+        ),
+        .package(
+          url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
+          from: "13.3.0"
+        ),
+      ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "GoogleMobileAdsMediationFluct"
+        name: "GoogleMobileAdsMediationFluct",
+        dependencies: [
+        .product(name: "FluctSDK", package: "FluctSDK-iOS-Swift-Package"),
+        .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+        ],
+        publicHeadersPath: "include"
         ),
         .testTarget(
             name: "GoogleMobileAdsMediationFluctTests",
-            dependencies: ["GoogleMobileAdsMediationFluct"]
+            dependencies: ["GoogleMobileAdsMediationFluct",
+                           .product(name: "FluctSDK", package: "FluctSDK-iOS-Swift-Package"),
+                           .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+                          ]
         ),
     ]
 )
